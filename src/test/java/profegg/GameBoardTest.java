d180f7d391f569d6d677baa988eaac9ce3fe2b23
Cryptic42069 <69015627+Cryptic42069@users.noreply.github.com>
2023-12-31 21:40:00 -0500

    Added some gamebaord tests

--- src/test/java/profegg/GameBoardTest.java
+++ src/test/java/profegg/GameBoardTest.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import profegg.playerclasses.Human;
@@ -13,5 +17,114 @@ import profegg.playerclasses.Human;
  */
 public class GameBoardTest {
 
+    @Test
+    public void correctMoveHistory1() {
+        ConnectFourBoard board = new ConnectFourBoard("456");
+        
+        List<Point> correctHistoryList = new ArrayList<>(Arrays.asList(new Point[] {
+            new Point(6, 4),
+            new Point(6, 5),
+            new Point(6, 6),
+        }));
+
+        assertTrue(board.copyMoveHistory().equals(correctHistoryList));
+    }
+
+    @Test
+    public void correctP1MoveHistory1() {
+        ConnectFourBoard board = new ConnectFourBoard("456");
+        
+        List<Point> correctHistoryList = new ArrayList<>(Arrays.asList(new Point[] {
+            new Point(6, 4),
+            new Point(6, 6),
+        }));
+
+        assertTrue(board.copyP1MoveHistory().equals(correctHistoryList));
+    }
     
+    @Test
+    public void correctP2MoveHistory1() {
+        ConnectFourBoard board = new ConnectFourBoard("456");
+        
+        List<Point> correctHistoryList = new ArrayList<>(Arrays.asList(new Point[] {
+            new Point(6, 5)
+        }));
+
+        assertTrue(board.copyP2MoveHistory().equals(correctHistoryList));
+    }
+
+    @Test
+    public void correctMoveHistoryWithUnadding1() {
+        ConnectFourBoard board = new ConnectFourBoard("4567");
+        board.unAddChip();
+        
+        List<Point> correctHistoryList = new ArrayList<>(Arrays.asList(new Point[] {
+            new Point(6, 4),
+            new Point(6, 5),
+            new Point(6, 6),
+        }));
+
+        assertTrue(board.copyMoveHistory().equals(correctHistoryList));
+    }
+
+    @Test
+    public void correctP1MoveHistoryWithUnadding1() {
+        ConnectFourBoard board = new ConnectFourBoard("4567");
+        board.unAddChip();
+        
+        List<Point> correctHistoryList = new ArrayList<>(Arrays.asList(new Point[] {
+            new Point(6, 4),
+            new Point(6, 6),
+        }));
+
+        assertTrue(board.copyP1MoveHistory().equals(correctHistoryList));
+    }
+    
+    @Test
+    public void correctP2MoveHistoryWithUnadding1() {
+        ConnectFourBoard board = new ConnectFourBoard("4567");
+        board.unAddChip();
+        
+        List<Point> correctHistoryList = new ArrayList<>(Arrays.asList(new Point[] {
+            new Point(6, 5)
+        }));
+
+        assertTrue(board.copyP2MoveHistory().equals(correctHistoryList));
+    }
+
+    @Test
+    public void playerOneToMove1() {
+        ConnectFourBoard board = new ConnectFourBoard("741253");
+        assertTrue(board.playerOneToMove());
+    }
+
+    @Test
+    public void playerOneToMove2() {
+        ConnectFourBoard board = new ConnectFourBoard();
+        assertTrue(board.playerOneToMove());
+    }
+
+    @Test
+    public void playerOneToMove3() {
+        ConnectFourBoard board = new ConnectFourBoard("4455");
+        board.unAddChip();
+        board.unAddChip();
+        assertTrue(board.playerOneToMove());
+    }
+
+    @Test
+    public void playerOneToMove4() {
+        ConnectFourBoard board = new ConnectFourBoard("4455");
+        board.unAddChip();
+        board.unAddChip();
+        assertTrue(board.playerOneToMove());
+    }
+
+    @Test
+    public void playerOneToMove5() {
+        ConnectFourBoard board = new ConnectFourBoard("4455");
+        board.unAddChip();
+        board.unAddChip();
+        assertTrue(board.playerOneToMove());
+    }
 }
