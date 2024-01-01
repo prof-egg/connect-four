3fe4d472de0cdaa21573c2a1f753f5294a46bfac
Cryptic42069 <69015627+Cryptic42069@users.noreply.github.com>
2023-12-31 21:40:25 -0500

    added equals method

--- src/main/java/profegg/Point.java
+++ src/main/java/profegg/Point.java
@@ -8,4 +8,16 @@ public final class Point {
         this.row = row;
         this.col = col;
     }
+
+    public boolean equals(Object otherObject) {
+        if (this == otherObject) return true;
+        if (otherObject == null) return false;
+        if (otherObject.getClass() != this.getClass()) return false;
+
+        Point that = (Point) otherObject;
+        if (this.row != that.row) return false;
+        if (this.col != that.col) return false;
+            
+        return true;
+    }
 }
